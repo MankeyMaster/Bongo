a33ddcbe157f92e95bfdce3042c946321508b431
MankeyMaster <50344257+MankeyMaster@users.noreply.github.com>
2020-01-06 21:00:11 -0700

    update and slight backend

--- Frontend/resources/spot.js
+++ Frontend/resources/spot.js
@@ -1,15 +1,59 @@
-'use strict'
+var spotifyApi = new SpotifyWebApi();
+const clientId = 'dc9dbbb0da88417b9e2da982fb163709';
+const clientSec = '463b923f1e8d4992ba595089a8222b33';
+const redirectUri = 'http://localhost:8888/callback';
+const scopes = ['streaming user-follow-modify user-follow-read playlist-read-private user-library-modify user-library-read playlist-modify-public user-read-playback-state'];
 
-import Client from '../src/Client';
-import UserHandler from '../src/handlers/UserHandler';
-import TrackHandler from '../src/handlers/TrackHandler';
-import PlaylistHandler from '../src/handlers/PlaylistHandler';
-import ArtistHandler from '../src/handlers/ArtistHandler';
+function login(callback) {
+  function getLoginURL() {
+      return 'https://accounts.spotify.com/authorize?client_id=' + clientId +
+        '&redirect_uri=' + encodeURIComponent(redirectUri) +
+        '&scope=' + encodeURIComponent(scopes) +
+        '&response_type=token';
+  }
+  
+  var url = getLoginURL([
+      'user-read-email'
+  ]);
+  
+  var width = 450,
+      height = 730,
+      left = (screen.width / 2) - (width / 2),
+      top = (screen.height / 2) - (height / 2);
 
-let client = Client.instance
+  window.addEventListener("message", function(event) {
+      var hash = JSON.parse(event.data);
+      if (hash.type == 'access_token') {
+          callback(hash.access_token);
+      }
+  }, false);
+  
+  var w = window.open(url,
+                      'Spotify',
+                      'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+                     );
+  
+}
 
-window.onSpotifyPlayerAPIReady = () => {
-    const player = new Spotify.Player({
-      name: 'Web Playback SDK Template',
-      getOAuthToken: cb => { cb(_token); }
-})};
+function getUserData(accessToken) {
+  return $.ajax({
+      url: 'https://api.spotify.com/v1/me',
+      headers: {
+         'Authorization': 'Bearer ' + accessToken
+      }
+  });
+}
+
+
+document.getElementById('login').addEventListener('click', function() {
+  login(function(accessToken) {
+      getUserData(accessToken)
+          .then(function(response) {
+              spotifyApi.setAccessToken(accessToken);
+          });
+      });
+});
+
+
+spotifyApi 
+
